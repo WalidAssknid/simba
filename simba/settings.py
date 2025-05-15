aba62a02f422251d1ec08bde45033e8cfebf9ffa
gabartas <gabartas@hotmail.fr>
2025-05-15 17:51:11 +0200

    add site to allowed hosts

--- simba/settings.py
+++ simba/settings.py
@@ -33,6 +33,7 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'db').split(',')
 
+CSRF_TRUSTED_ORIGINS = ['https://simba-refact.irit.fr']
 
 # Application definition
 

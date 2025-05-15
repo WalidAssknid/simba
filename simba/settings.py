e988315e80597b2349f5125605e9bed927b7eb38
gabartas <gabartas@hotmail.fr>
2025-05-15 16:35:36 +0200

    DB port change

--- simba/settings.py
+++ simba/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'simba_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'secretpassword'),
         'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': os.getenv('DB_PORT', '5433'),
     }
 }
 
